a2096f7bc73cff33f0069abd56da48ae00314c56
omaryahia4 <omar.yahia4444@gmail.com>
2021-06-30 22:47:54 +0000

    pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,10 @@
 
 import json
 
+
 class FileStorage:
-    """serializes instances to a JSON file and deserializes JSON file to instances"""
+    """serializes instances to a JSON file
+    and deserializes JSON file to instances"""
     __file_path = "file.json"
     __objects = {}
 
@@ -16,7 +18,7 @@ class FileStorage:
         """sets in __objects the obj with key <obj class name>.id"""
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
-    
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         JS = {}
