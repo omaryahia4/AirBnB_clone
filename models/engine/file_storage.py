2b003d5e2d87e1ba4847db441117e524ae724672
omaryahia4 <omar.yahia4444@gmail.com>
2021-07-01 21:42:45 +0000

    task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """Module Storage """
 
+from models.base_model import BaseModel
 import json
+from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.city import City
+from models.review import Review
 
 
 class FileStorage:
