bafbfaec9d98ae676e0a900f4706fa48a506a5ed
omaryahia4 <omar.yahia4444@gmail.com>
2021-07-01 12:01:28 +0000

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Module base """
 
-import uuid 
+import uuid
 from datetime import datetime
 import models
+
 
 class BaseModel:
     """Base class"""
@@ -14,27 +15,33 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['created_at'] = datetime.strptime
+            (kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['updated_at'] = datetime.strptime
+            (kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
             for key, value in kwargs.items():
                 if key is not "__class__":
                     setattr(self, key, value)
         else:
             models.storage.new(self)
+
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format
+        (self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute
+         updated_at with the current datetime"""
 
         self.updated_at = datetime.now()
         models.storage.save()
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all
+         keys/values of __dict__ of the instance"""
 
         Dict = self.__dict__
         Dict["__class__"] = self.__class__.__name__
         Dict["created_at"] = self.created_at.isoformat()
         Dict["updated_at"] = self.updated_at.isoformat()
         return Dict
-
