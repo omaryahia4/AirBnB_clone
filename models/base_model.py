68a5b7191530611056bf9f0c375d31d91f74070f
omaryahia4 <77851226+omaryahia4@users.noreply.github.com>
2021-07-29 10:41:27 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -30,13 +30,13 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute
-         updated_at with the current datetime"""
+        updated_at with the current datetime"""
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all
-         keys/values of __dict__ of tcdhe instance"""
+        keys/values of __dict__ of tcdhe instance"""
         Dict = self.__dict__
         Dict["__class__"] = self.__class__.__name__
         Dict["created_at"] = self.created_at.isoformat()
